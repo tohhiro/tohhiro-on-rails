7a72d33595ecd30cccf551ffa61365f35c07c7d2
tohhiro <tohhiro@gmail.com>
2025-12-16 08:47:47 +0900

    postsページを追加し、.erbの自動フォーマットを無効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root to: 'posts#index', as: 'posts' # posts_path
+  get '/posts/:id', to: 'posts#show', as: 'post' # post_path(id)
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
